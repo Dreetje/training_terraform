aec3c96fc1808fd368082fa50d9a15c3d741c539
Dreetje <43443611+Dreetje@users.noreply.github.com>
2023-10-20 15:10:31 +0200

    Update github.tf

--- github.tf
+++ github.tf
@@ -3,18 +3,15 @@ provider "aws" {
     profile = "dev_engineer"
 }
 
-data "aws_security_group" "sg_terraform" {
-  id = "sg-074868af41ded3c9d"
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
 }
 
-resource "aws_instance" "avi-terraform-03" {
-    ami = "ami-0c9c5a0ab4a28ec61"
-    instance_type = "t2.micro"
-    subnet_id = "subnet-033b9bdd609940306"
-    iam_instance_profile = "avi-iam-role"
-    vpc_security_group_ids = [data.aws_security_group.sg_terraform.id]
-
-    tags = {
-        Name = "avi-terraform-03"
-    }
+provider "github" {
+  token = "your-token-here"
 }
